ed7f41ed2ebaca25601f63e5aa773149bd2187f6
Sean Murphy <sean@iamseanmurphy.com>
2026-03-03 01:48:04 +0000

    1.7.3

--- dynamic-month-year-into-posts/build/blocks/published-date/index.asset.php
+++ dynamic-month-year-into-posts/build/blocks/published-date/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n', 'wp-primitives'), 'version' => '44ab970c8db1132ed397');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n', 'wp-primitives'), 'version' => '6756f85b31caa38388a2');
--- dynamic-month-year-into-posts/build/index.asset.php
+++ dynamic-month-year-into-posts/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '24f240464c6e33bf9226');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'f40ed7fca1a906d9e082');
